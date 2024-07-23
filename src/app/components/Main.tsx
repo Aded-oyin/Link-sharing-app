39778a2b87c89353d8b9c0ef19de4114d3f0d657
Aded-oyin <danyodele50@gmail.com>
2024-07-23 20:21:27 +0100

    test

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -28,15 +28,16 @@ const Main = () => {
             </div>
         </div>
 
+        {/*Mobile*/}
         <div>
-            <div className="lg:hidden bg-white w-[500px] md:w-[721px] mx-auto">
-                <div className="w-[295px] md:w-[641px] mx-auto m-7">
+            <div className="lg:hidden bg-white w-full md:w-[721px] mx-auto">
+                <div className="w-[295px] md:w-[600px] mx-auto m-7">
                     <h1 className="text-2xl font-bold">Customize your Links</h1>
                     <p className="text-[16px] text-[#737373] pt-5">Add/edit/remove links below and then share all your profiles with the world!</p>
-                    <button className="w-[295px] md:w-[641px] h-[46px] border border-[#633CFF] rounded-md mt-10 hover:bg-[#EFEBFF] text-[#633CFF]">
+                    <button className="w-[295px] md:w-[600px] h-[46px] border border-[#633CFF] rounded-md mt-10 hover:bg-[#EFEBFF] text-[#633CFF]">
                         + Add new Link
                     </button>
-                    <div className="bg-[#FAFAFA] w-[295px] md:w-[641px] h-[400px] mt-20 flex flex-col justify-center items-center">
+                    <div className="bg-[#FAFAFA] w-[295px] md:w-[600px] h-[400px] mt-20 flex flex-col justify-center items-center">
                         <Image src={Phones} alt="phones"/>
                         <div className="text-center items-center md:w-[470px]">
                             <h1 className="text-2xl font-bold">{"Let's"} get you started</h1>
