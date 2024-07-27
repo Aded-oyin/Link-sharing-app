5f7fda401a934a22d6d54ee15be13e91df3fa5ff
Aded-oyin <danyodele50@gmail.com>
2024-07-27 10:43:57 +0100

    fix-responsiveness

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -33,19 +33,19 @@ const Login = () => {
 
     return (
         <div>
-            <div className='mx-auto mt-10 w-[480px] h-[573px]'>
+            <div className='mx-auto mt-10 w-[400px] md:w-[480px] h-[573px]'>
                 <h1 className="font-bold text-[40px] flex items-center pl-[120px]">
                     <Image src={Logo} alt="logo" width={40} height={40} />
                     <span>devlinks</span>
                 </h1>
-                <div className='w-[476px] h-[530px] mt-7 pb-[30px] bg-white'>
-                    <div className='max-w-[400px] mx-auto py-16'>
+                <div className='w-[380px] md:w-[476px] h-[530px] mt-7 pb-[30px] bg-white'>
+                    <div className='w-[320px] md:w-[400px] mx-auto py-16'>
                         <h1 className='text-3xl font-bold'>Login</h1>
                         <p className="text-[#737373] py-4">Add your details below to get back into the app</p>
                         <form className='w-full flex flex-col pt-7' onSubmit={handleSubmit}>
                             <p className='text-[14px]'>Email address</p>
                             <input
-                                className='my-2 border rounded-md w-[396px] h-[48px] hover:border-[#633CFF]'
+                                className='my-2 border rounded-md w-[311px] md:w-[396px] h-[48px] hover:border-[#633CFF]'
                                 type='email'
                                 placeholder='e.g. alex@email.com'
                                 value={email}
@@ -54,7 +54,7 @@ const Login = () => {
                             />
                             <p className="mt-5 text-[14px]">Password</p>
                             <input
-                                className='my-2 border rounded-md w-[396px] h-[48px] hover:border-[#633CFF]'
+                                className='my-2 border rounded-md w-[311px] md:w-[396px] h-[48px] hover:border-[#633CFF]'
                                 type='password'
                                 placeholder='Enter your password'
                                 value={password}
@@ -62,7 +62,7 @@ const Login = () => {
                                 required
                             />
                             <button
-                                className='bg-[#633CFF] w-[396px] h-[48px] py-3 my-6 rounded-md font-bold text-white'
+                                className='bg-[#633CFF] w-[311px] md:w-[396px] h-[48px] py-3 my-6 rounded-md font-bold text-white'
                                 type='submit'
                                 disabled={loading}
                             >
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -40,19 +40,19 @@ const Signup = () => {
 
     return (
         <div>
-            <div className='mx-auto mt-10 w-[480px] h-[709px]'>
+            <div className='mx-auto mt-10 w-[400px] md:w-[480px] h-[709px]'>
                 <h1 className="font-bold text-[40px] flex items-center pl-[120px]">
                     <Image src={Logo} alt="logo" width={40} height={40} />
                     <span>devlinks</span>
                 </h1>
-                <div className='w-[476px] h-[618px] mt-7 pb-[30px] bg-white'>
-                    <div className='max-w-[400px] mx-auto py-16'>
+                <div className='w-[380px] md:w-[476px] h-[618px] mt-7 pb-[30px] bg-white'>
+                    <div className='w-[320px] md:w-[400px] mx-auto py-16'>
                         <h1 className='text-3xl font-bold text-[#333333] font-sans'>Create account</h1>
                         <p className="text-[#737373] py-4">{"Let's"} get you started sharing your links!</p>
                         <form className='w-full flex flex-col pt-7' onSubmit={handleSubmit}>
                             <p className='text-[14px]'>Email address</p>
                             <input
-                                className='my-2 border rounded-md w-[396px] h-[48px] hover:border-[#633CFF] duration-200 '
+                                className='my-2 border rounded-md w-[311px] md:w-[396px] h-[48px] hover:border-[#633CFF] duration-200 '
                                 type='email'
                                 placeholder='e.g. alex@email.com'
                                 value={email}
@@ -61,7 +61,7 @@ const Signup = () => {
                             />
                             <p className="mt-5 text-[14px]">Create password</p>
                             <input
-                                className='my-2 border rounded-md w-[396px] h-[48px] hover:border-[#633CFF]'
+                                className='my-2 border rounded-md w-[311px] md:w-[396px] h-[48px] hover:border-[#633CFF]'
                                 type='password'
                                 placeholder='At least 8 characters'
                                 value={password}
@@ -70,7 +70,7 @@ const Signup = () => {
                             />
                             <p className="mt-5 text-[14px]">Confirm password</p>
                             <input
-                                className='my-2 border rounded-md w-[396px] h-[48px] hover:border-[#633CFF]'
+                                className='my-2 border rounded-md w-[311px] md:w-[396px] h-[48px] hover:border-[#633CFF]'
                                 type='password'
                                 placeholder='At least 8 characters'
                                 value={confirmPassword}
@@ -79,7 +79,7 @@ const Signup = () => {
                             />
                             {error && <p className="text-red-500">{error}</p>}
                             <button
-                                className='bg-[#633CFF] w-[396px] h-[48px] py-3 my-6 rounded-md font-bold text-white'
+                                className='bg-[#633CFF] w-[311px] md:w-[396px] h-[48px] py-3 my-6 rounded-md font-bold text-white'
                                 type='submit'
                                 disabled={loading}
                             >
